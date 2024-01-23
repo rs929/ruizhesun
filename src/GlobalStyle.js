3299d90fa360825ebe673743922a57ebed5318cf
Richie Sun <rs929@cornell.edu>
2024-01-23 10:31:03 -0500

    Added nav Bar

---
+++ src/GlobalStyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+:root {
+    --background: #FFFFFF:
+    --lavendar: #C589E8;
+    --purple: #410f70;
+    --dark-purple: #220b4e;
+    --light-purple: #b691ff;
+    --offblack: #232b2b;
+    --hover: rgba(0, 0, 0, 0.1);
+}
+
+html {
+    font-size: 16px;
+    font-family: Georgia;
+    background-color: var(--background);
+}
+
+ul, li {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { render } from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
+import GlobalStyle from './GlobalStyle'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.querySelector("#root")
 )
+
+reportWebVitals();
